e37713d1ff80c855ce2a704cc37ecbae5b7f24aa
Hans <hans.harvey@h2hteam.com>
2015-07-21 21:20:09 +0100

    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# The makeCacheMatrix function creates and returns a list of  4 functions
+# used by cacheSolve to get or set the inverted matrix in cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      # Set the value of inv as NULL. Default value if the cacheSolve was not used before
+      inv <- NULL
+      
+      # Function to set the value of the matrix
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      
+      # Function to return the input matrix
+      get <- function() x
+      # Function to set the inv to the inversed matrix - with the solve() function
+      setInverse <- function(inverse) inv <<- inverse
+      # Function to return the inversed matrix
+      getInverse <- function() inv
+      # Set a list of function to be used as x$function
+      list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      # Function to get the inversed matrix value
+	inv <- x$getInverse()
+	# Return the value the inversed martix-value if not NULL: 
+	
+	if(!is.null(inv)) {
+      	message("getting cached data.")
+      	return(inv)
+	}
+	
+	# Get the value from the matrix
+      cache_dat <- x$get()
+      # Compute the inverse of the matrix value
+      inv <- solve(cache_dat)
+      # Function to set the value of the cache object.
+      x$setInverse(inv)
+      inv
 }
